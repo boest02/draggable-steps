dca17c73441bfadc76c2fa31f118158a85bacc56
Boettcher, Stephen <sboettcher@pch.com>
2021-01-23 23:35:40 -0500

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let name;
+</script>
+
+
+<h1>Hello {name}!</h1>
+
+<div>
+  <p>SASS is working!</p>
+</div>
+
+
+<style type="text/scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+    padding: 20px;
+  }
+
+  div {
+    background: green;
+    width: 90%;
+    padding: 15px;
+    margin: 10px auto;
+
+    > p {
+      color: #fff;
+      font-weight: 800;
+      font-size: 1em;
+      text-align: center;
+    }
+  }
+</style>
+
